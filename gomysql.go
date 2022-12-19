6f1e67022e2177c0ddfa179219b1fdefb0817010
Abhishek Salian <abhi@illuminasy.com>
2022-12-19 23:45:04 +1000

    Added newrelic

--- gomysql.go
+++ gomysql.go
@@ -1,11 +1,14 @@
 package gomysql
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+
+	_ "github.com/newrelic/go-agent/v3/integrations/nrmysql"
 )
 
 // MaxOpenConns maximum open connections
@@ -37,6 +40,7 @@ type Config struct {
 	MaxIdleConns     int
 	MaxOpenConnsTime time.Duration
 	MaxIdleConnsTime time.Duration
+	NewrelicEnabled  bool
 }
 
 // Client ...
@@ -54,10 +58,19 @@ type Datastore interface {
 	QueryRow(query string, params []interface{}) (*sql.Row, error)
 	Exec(query string, params []interface{}) (sql.Result, error)
 
+	QueryWithContext(ctx context.Context, query string, params []interface{}) (*sql.Rows, error)
+	QueryRowWithContext(ctx context.Context, query string, params []interface{}) (*sql.Row, error)
+	ExecWithContext(ctx context.Context, query string, params []interface{}) (sql.Result, error)
+
 	Prepare(query string) (*sql.Stmt, error)
 	StmtQuery(stmt *sql.Stmt, params []interface{}) (*sql.Rows, error)
 	StmtQueryRow(stmt *sql.Stmt, params []interface{}) *sql.Row
 	StmtExec(stmt *sql.Stmt, params []interface{}) (sql.Result, error)
+
+	PrepareWithContext(ctx context.Context, query string) (*sql.Stmt, error)
+	StmtQueryWithContext(ctx context.Context, stmt *sql.Stmt, params []interface{}) (*sql.Rows, error)
+	StmtQueryRowWithContext(ctx context.Context, stmt *sql.Stmt, params []interface{}) *sql.Row
+	StmtExecWithContext(ctx context.Context, stmt *sql.Stmt, params []interface{}) (sql.Result, error)
 
 	BeginTx() (*sql.Tx, error)
 	TxPrepare(tx *sql.Tx, query string) (*sql.Stmt, error)
@@ -66,6 +79,12 @@ type Datastore interface {
 	TxExec(tx *sql.Tx, query string, params []interface{}) (sql.Result, error)
 	TxCommit(tx *sql.Tx) error
 	TxRollback(tx *sql.Tx) error
+
+	BeginTxWithContext(ctx context.Context) (*sql.Tx, error)
+	TxPrepareWithContext(ctx context.Context, tx *sql.Tx, query string) (*sql.Stmt, error)
+	TxQueryWithContext(ctx context.Context, tx *sql.Tx, query string, params []interface{}) (*sql.Rows, error)
+	TxQueryRowWithContext(ctx context.Context, tx *sql.Tx, query string, params []interface{}) *sql.Row
+	TxExecWithContext(ctx context.Context, tx *sql.Tx, query string, params []interface{}) (sql.Result, error)
 
 	Migrate() error
 }
@@ -94,6 +113,23 @@ func (c Client) ConnCheck() (bool, error) {
 	return false, err
 }
 
+// ConnCheck check if connection exists
+func (c Client) ConnCheckWithContext(ctx context.Context) (bool, error) {
+	dbConn, err := getDbConn(c.config)
+	if err != nil {
+		return false, err
+	}
+
+	defer dbConn.Close()
+
+	err = dbConn.PingContext(ctx)
+	if err == nil {
+		return true, nil
+	}
+
+	return false, err
+}
+
 // GetStats return database stats
 func (c Client) GetStats() (sql.DBStats, error) {
 	dbConn, err := getDbConn(c.config)
@@ -114,6 +150,16 @@ func (c Client) Query(query string, params []interface{}) (*sql.Rows, error) {
 	return dbConn.Query(query, params...)
 }
 
+// QueryWithContext queries multiple rows
+func (c Client) QueryWithContext(ctx context.Context, query string, params []interface{}) (*sql.Rows, error) {
+	dbConn, err := getDbConn(c.config)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn.QueryContext(ctx, query, params...)
+}
+
 // QueryRow Execute query and returns single row
 func (c Client) QueryRow(query string, params []interface{}) (*sql.Row, error) {
 	dbConn, err := getDbConn(c.config)
@@ -122,6 +168,16 @@ func (c Client) QueryRow(query string, params []interface{}) (*sql.Row, error) {
 	}
 
 	return dbConn.QueryRow(query, params...), nil
+}
+
+// QueryRowWithContext Execute query and returns single row
+func (c Client) QueryRowWithContext(ctx context.Context, query string, params []interface{}) (*sql.Row, error) {
+	dbConn, err := getDbConn(c.config)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn.QueryRowContext(ctx, query, params...), nil
 }
 
 // Exec ...
@@ -134,6 +190,16 @@ func (c Client) Exec(query string, params []interface{}) (sql.Result, error) {
 	return dbConn.Exec(query, params...)
 }
 
+// ExecWithContext ...
+func (c Client) ExecWithContext(ctx context.Context, query string, params []interface{}) (sql.Result, error) {
+	dbConn, err := getDbConn(c.config)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn.ExecContext(ctx, query, params...)
+}
+
 // Prepare Prepare query
 func (c Client) Prepare(query string) (*sql.Stmt, error) {
 	dbConn, err := getDbConn(c.config)
@@ -144,9 +210,24 @@ func (c Client) Prepare(query string) (*sql.Stmt, error) {
 	return dbConn.Prepare(query)
 }
 
+// PrepareWithContext Prepare query
+func (c Client) PrepareWithContext(ctx context.Context, query string) (*sql.Stmt, error) {
+	dbConn, err := getDbConn(c.config)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn.PrepareContext(ctx, query)
+}
+
 // StmtQuery queries multiple rows
 func (c Client) StmtQuery(stmt *sql.Stmt, params []interface{}) (*sql.Rows, error) {
 	return stmt.Query(params...)
+}
+
+// StmtQueryWithContext queries multiple rows
+func (c Client) StmtQueryWithContext(ctx context.Context, stmt *sql.Stmt, params []interface{}) (*sql.Rows, error) {
+	return stmt.QueryContext(ctx, params...)
 }
 
 // StmtQueryRow Execute query and returns single row
@@ -154,9 +235,19 @@ func (c Client) StmtQueryRow(stmt *sql.Stmt, params []interface{}) *sql.Row {
 	return stmt.QueryRow(params...)
 }
 
+// StmtQueryRowWithContext Execute query and returns single row
+func (c Client) StmtQueryRowWithContext(ctx context.Context, stmt *sql.Stmt, params []interface{}) *sql.Row {
+	return stmt.QueryRowContext(ctx, params...)
+}
+
 // StmtExec Execute query
 func (c Client) StmtExec(stmt *sql.Stmt, params []interface{}) (sql.Result, error) {
 	return stmt.Exec(params...)
+}
+
+// StmtExecWithContext Execute query
+func (c Client) StmtExecWithContext(ctx context.Context, stmt *sql.Stmt, params []interface{}) (sql.Result, error) {
+	return stmt.ExecContext(ctx, params...)
 }
 
 // TxBegin Start and return transaction
@@ -169,9 +260,24 @@ func (c Client) TxBegin() (*sql.Tx, error) {
 	return dbConn.Begin()
 }
 
+// TxBeginWithContext Start and return transaction
+func (c Client) TxBeginWithContext(ctx context.Context) (*sql.Tx, error) {
+	dbConn, err := getDbConn(c.config)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn.BeginTx(ctx, nil)
+}
+
 // TxQuery queries multiple rows
 func (c Client) TxQuery(tx *sql.Tx, query string, params []interface{}) (*sql.Rows, error) {
 	return tx.Query(query, params...)
+}
+
+// TxQueryWithContext queries multiple rows
+func (c Client) TxQueryWithContext(ctx context.Context, tx *sql.Tx, query string, params []interface{}) (*sql.Rows, error) {
+	return tx.QueryContext(ctx, query, params...)
 }
 
 // TxQueryRow Execute query and returns single row
@@ -179,9 +285,19 @@ func (c Client) TxQueryRow(tx *sql.Tx, query string, params []interface{}) *sql.
 	return tx.QueryRow(query, params...)
 }
 
+// TxQueryRowWithContext Execute query and returns single row
+func (c Client) TxQueryRowWithContext(ctx context.Context, tx *sql.Tx, query string, params []interface{}) *sql.Row {
+	return tx.QueryRowContext(ctx, query, params...)
+}
+
 // TxExec Execute query
 func (c Client) TxExec(tx *sql.Tx, query string, params []interface{}) (sql.Result, error) {
 	return tx.Exec(query, params...)
+}
+
+// TxExecWithContext Execute query
+func (c Client) TxExecWithContext(ctx context.Context, tx *sql.Tx, query string, params []interface{}) (sql.Result, error) {
+	return tx.ExecContext(ctx, query, params...)
 }
 
 // TxCommit commits transaction
@@ -207,7 +323,13 @@ func getDbConn(config Config) (*sql.DB, error) {
 		buildParameters(config),
 	)
 
-	db, err := sql.Open("mysql", mysqlSource)
+	driverName := "mysql"
+
+	if config.NewrelicEnabled {
+		driverName = "nrmysql"
+	}
+
+	db, err := sql.Open(driverName, mysqlSource)
 	if err != nil {
 		return nil, err
 	}
